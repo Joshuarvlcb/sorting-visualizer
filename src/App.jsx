06843528527d3a994d62ad46556029a6ad3006ee
Saul Ruvalcaba <joshuarvlcb@gmail.com>
2021-12-03 11:01:05 -0700

    finished bubble sort

--- src/App.jsx
+++ src/App.jsx
@@ -42,50 +42,45 @@ function App() {
       />
       <button
         onClick={() => {
-          const items = bubbleSort(array);
-          const sync = (color, id, items) => {
-            setTimeout(() => {
-              items[id].style.backgroundColor = color;
-            }, 1000);
-          };
-          let pointer = 0;
-          for (let [i, id] of Object.entries(items)) {
-            const items = [...document.getElementsByClassName("item")];
+          const DOMitems = [...document.getElementsByClassName("item")];
 
-            console.log(id[2], id[1]);
-            // items[id[0]].style.backgroundColor = "red";
-            // if (id[1] == id[2]) {
-            //   setTimeout(() => {
-            //     items[id[0]].style.backgroundColor = "red";
-            //   }, i * 10);
-            //   pointer++;
-            //   console.log(pointer);
-            // } else {
-            //   setTimeout(() => {
-            //     items[id[0]].style.backgroundColor = "black";
-            //   }, i * 10);
-            // }
+          bubbleSort(array, DOMitems);
 
-            if (id.includes("swap")) {
-              setTimeout(() => {
-                items[id[0]].style.height = id[3] + "%";
-                items[id[1]].style.height = id[2] + "%";
-                items[id[0]].style.backgroundColor = "green";
-                items[id[1]].style.backgroundColor = "blue";
-                setTimeout(function () {
-                  items[id[0]].style.backgroundColor = "red";
-                  items[id[1]].style.backgroundColor = "red";
-                }, 100);
-              }, i * 50);
-            } else {
-              if (id[1] == id[2]) {
-                sync("red", id[0], items);
-                console.log(pointer);
-              } else {
-                sync("black", id[0], items);
-              }
-            }
-            /*
+          // for (let [i, id] of Object.entries(items)) {
+
+          // items[id[0]].style.backgroundColor = "red";
+          // if (id[1] == id[2]) {
+          //   setTimeout(() => {
+          //     items[id[0]].style.backgroundColor = "red";
+          //   }, i * 10);
+          //   pointer++;
+          //   console.log(pointer);
+          // } else {
+          //   setTimeout(() => {
+          //     items[id[0]].style.backgroundColor = "black";
+          //   }, i * 10);
+          // }
+
+          // if (id.includes("swap")) {
+          //   setTimeout(() => {
+          //     items[id[0]].style.height = id[3] + "%";
+          //     items[id[1]].style.height = id[2] + "%";
+          //     items[id[0]].style.backgroundColor = "green";
+          //     items[id[1]].style.backgroundColor = "blue";
+          //     setTimeout(function () {
+          //       items[id[0]].style.backgroundColor = "red";
+          //       items[id[1]].style.backgroundColor = "red";
+          //     }, 100);
+          //   }, i * 50);
+          // } else {
+          //   if (id[1] == id[2]) {
+          //     sync("red", id[0], items);
+          //     console.log(pointer);
+          //   } else {
+          //     sync("black", id[0], items);
+          //   }
+          // }
+          /*
             
             30 is black 
             when j = 30 set 30 = red
@@ -93,19 +88,19 @@ function App() {
             when j == 29
   
             */
-            // if (id[1] == array.length - 1) {
-            //   setTimeout(() => {
-            //     items[keepTrackOfI].style.backgroundColor = "red";
-            //   }, i * 50);
-            //   keepTrackOfI.push(id[0]);
-            // } else {
-            //   setTimeout(() => {
-            //     items[id[0]].style.backgroundColor = "black";
-            //     pointer++;
-            //   }, i * 50);
-            // }
+          // if (id[1] == array.length - 1) {
+          //   setTimeout(() => {
+          //     items[keepTrackOfI].style.backgroundColor = "red";
+          //   }, i * 50);
+          //   keepTrackOfI.push(id[0]);
+          // } else {
+          //   setTimeout(() => {
+          //     items[id[0]].style.backgroundColor = "black";
+          //     pointer++;
+          //   }, i * 50);
+          // }
 
-            /*
+          /*
               show where i is
               i need to change i back to red when we finished the iteration of the array
               get values that are not swapped and % arr.length == 0
@@ -113,69 +108,68 @@ function App() {
               for each iteration i want to check to see if we chnage i to red
               show what we are comparing
             */
-            // if (!id.includes("swap")) {
-            //   keepTrackOfI++;
-            // }
-            //animatioms
-            // const index = document.getElementById(id[0]).style;
-            // const j = document.getElementById(id[1]).style;
-            //toggle colors
-            // if (id.includes("swap")) {
-            //   setTimeout(() => {
-            //     items[id[0]].style.height = id[3] + "%";
-            //     items[id[1]].style.height = id[2] + "%";
-            //     // items[id[0]].style.backgroundColor = "green";
-            //     // items[id[1]].style.backgroundColor = "blue";
-            //     setTimeout(() => {
-            //       items[id[0]].style.backgroundColor = "red";
-            //       items[id[1]].style.backgroundColor = "red";
-            //     }, 1 * i);
-            //   }, i * 100);
-            // } else {
-            //   setTimeout(() => {
-            //     if (items[id[1]] && items[id[0]]) {
-            //       items[id[1]].style.backgroundColor = "pink";
+          // if (!id.includes("swap")) {
+          //   keepTrackOfI++;
+          // }
+          //animatioms
+          // const index = document.getElementById(id[0]).style;
+          // const j = document.getElementById(id[1]).style;
+          //toggle colors
+          // if (id.includes("swap")) {
+          //   setTimeout(() => {
+          //     items[id[0]].style.height = id[3] + "%";
+          //     items[id[1]].style.height = id[2] + "%";
+          //     // items[id[0]].style.backgroundColor = "green";
+          //     // items[id[1]].style.backgroundColor = "blue";
+          //     setTimeout(() => {
+          //       items[id[0]].style.backgroundColor = "red";
+          //       items[id[1]].style.backgroundColor = "red";
+          //     }, 1 * i);
+          //   }, i * 100);
+          // } else {
+          //   setTimeout(() => {
+          //     if (items[id[1]] && items[id[0]]) {
+          //       items[id[1]].style.backgroundColor = "pink";
 
-            //       items[id[0]].style.backgroundColor = "black";
+          //       items[id[0]].style.backgroundColor = "black";
 
-            //       setTimeout(() => {
-            //         // items[id[0]].style.backgroundColor = "red";
-            //         items[id[1]].style.backgroundColor = "red";
-            //       }, 1 * i);
-            //     }
-            //   }, i * 100);
+          //       setTimeout(() => {
+          //         // items[id[0]].style.backgroundColor = "red";
+          //         items[id[1]].style.backgroundColor = "red";
+          //       }, 1 * i);
+          //     }
+          //   }, i * 100);
 
-            /*
+          /*
 
             */
-            // setTimeout(() => {
-            //   ii++;
-            //   console.log(ii);
-            //   items.find((elem) => elem.id == id[0]).style.height =
-            //     id[1] + "px";
-            //   items.find((elem) => elem.id == id[1]).style.height =
-            //     id[0] + "px";
-            // }, i * 100);
+          // setTimeout(() => {
+          //   ii++;
+          //   console.log(ii);
+          //   items.find((elem) => elem.id == id[0]).style.height =
+          //     id[1] + "px";
+          //   items.find((elem) => elem.id == id[1]).style.height =
+          //     id[0] + "px";
+          // }, i * 100);
 
-            // items[i].style.height = 100 + "px";
+          // items[i].style.height = 100 + "px";
 
-            // if (reset) {
-            //   // setTimeout(() => {
-            //   //   index.backgroundColor = "blue";
-            //   //   j.backgroundColor = "black";
-            //   // }, i * 50);
-            // } else {
-            //   setTimeout(() => {
-            //     index.backgroundColor = "red";
-            //     index.height = id[1] + "px";
-            //     j.height = id[0] + "px";
-            //   }, i * 100);
-            // }
+          // if (reset) {
+          //   // setTimeout(() => {
+          //   //   index.backgroundColor = "blue";
+          //   //   j.backgroundColor = "black";
+          //   // }, i * 50);
+          // } else {
+          //   setTimeout(() => {
+          //     index.backgroundColor = "red";
+          //     index.height = id[1] + "px";
+          //     j.height = id[0] + "px";
+          //   }, i * 100);
+          // }
 
-            /*
+          /*
           
           */
-          }
         }}
       >
         bubble sort
--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,30 +1,134 @@
-export const bubbleSort = (arr, updateArr, tick) => {
-  let i = arr.length - 1;
-  let j = 0;
-  let animations = [];
+export const bubbleSort = async (arr, items) => {
+  let iteration = {
+    i: 0,
+    j: 1,
+    sorted: true,
+    done: 0,
+  };
+
   //keep track of i and j
   //keep track of finsihed values
   //   let arrClone = arr.slice();
-  //   while (true) {
-  //     //4 steps
-  //     // animations.push([arr[i], arr[j]]);
 
-  //     if (arr[j] > arr[i]) {
-  //       let temp = arr[i];
-  //       arr[i] = arr[j];
-  //       arr[j] = temp;
-  //     }
-  //     if (j === i) {
-  //       i--;
-  //       j = 0;
-  //     } else {
-  //       j++;
-  //     }
-  //     //return case
-  //     if (i === 0) {
-  //       break;
-  //     }
-  //   }
+  let j = 0;
+  const done = async () => {
+    let hi = new Promise(function (resolve, reject) {
+      setTimeout(() => {
+        if (iteration.sorted === "done") resolve(true);
+        else reject(false);
+      }, j * 40);
+    });
+    return hi;
+  };
+  let index = 0;
+  let stop = false;
+  while (index < 600) {
+    index++;
+    j++;
+    /*
+        reset color meaning clear previous background colors;
+        reset i and j after they hot arr,length and check to see if we made any changes;
+  
+        fix j bug 
+        fix colors
+        make it sort properly
+        have a state to end
+  
+      we are dragging greatest value at the end 
+     [30,23,455,24];
+     let i = arr[i];
+     let j  = arr[i + 1]
+  if(i < j){
+    i = j 
+    j = next
+  }
+  30 > 23
+  items[j] = i
+  items[i] = j
+  
+      compare i and j
+      move up i and j
+      placed when value is placed at the end hightlight it green
+      if we are done sorting break
+        */
+    /*
+        items
+        arr[itrations] 
+  
+        swap
+        we never swap the values in our array
+       */
+
+    ((j, iteration) => {
+      setTimeout(() => {
+        if (arr[iteration.j] < arr[iteration.i]) {
+          // console.log(items[iteration.j], items[iteration.i]);
+          // console.log(arr[iteration.j], arr[iteration.i]);
+          /*
+          [40,30]
+          30,40
+          40 = 30
+          30 = 40
+  */ iteration.sorted = true;
+          let temp = arr[iteration.j];
+          arr[iteration.j] = arr[iteration.i];
+          arr[iteration.i] = temp;
+          items[iteration.i].style.height = arr[iteration.i] + "%";
+          items[iteration.j].style.height = arr[iteration.j] + "%";
+        }
+        if (iteration.j === arr.length - (1 + iteration.done) ) {
+          items[iteration.j].style.backgroundColor = "red";
+          iteration.j = 1;
+          iteration.done++;
+          console.log(iteration.done)
+          if (iteration.sorted) iteration.sorted = false;
+          else {
+            iteration.sorted = "done";
+          }
+        } else {
+          console.log(iteration.done)
+
+          iteration.j++;
+          items[iteration.j].style.backgroundColor = "black";
+        }
+
+        if (iteration.i === arr.length - (2 + iteration.done) ) {
+          items[iteration.i].style.backgroundColor = "red";
+          iteration.i = 0;
+          console.log(iteration.done)
+
+        } else {
+          items[iteration.i].style.backgroundColor = "red";
+          iteration.i++;
+          items[iteration.i].style.backgroundColor = "blue";
+        }
+      }, j * 100);
+    })(j, iteration);
+  }
+
+  //   // await compare();
+
+  //   // setTimeout(() => {
+  //   //   items[i].style.backgroundColor = "red";
+  //   //   items[i].style.backgroundColor = "red";
+  //   // }, 100);
+  //   // await new Promise((resolve) => {
+  //   //   if (i == arr.length - 1) {
+  //   //     i = 0;
+  //   //     resolve();
+  //   //   }
+  //   // });
+  //   // await new Promise((resolve) => {
+  //   //   if (j == arr.length - 1) {
+  //   //     j = 0;
+  //   //     resolve();
+  //   //   }
+  //   // });
+  //   // await new Promise((resolve) => {
+  //   //   index++;
+  //   //   resolve();
+  //   // });
+  // }
 
   /*
     we want to set a timeout for the animation 
@@ -32,19 +136,18 @@ export const bubbleSort = (arr, updateArr, tick) => {
     use while loop
 
     */
-  let counter = 0;
-  for (let i = arr.length; i > 0; i--) {
-    counter++;
-    for (let j = 0; j < i; j++) {
-      // animations.push([i - 1, j, arr.length - counter]);
-      if (arr[j] > arr[i]) {
-        let temp = arr[i];
-        let tj = arr[j];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        animations.push([i, j, temp, tj, "swap"]);
-      }
-    }
-  }
-  return animations;
+  // let counter = 0;
+  // for (let i = arr.length; i > 0; i--) {
+  //   counter++;
+  //   for (let j = 0; j < i; j++) {
+  //     // animations.push([i - 1, j, arr.length - counter]);
+  //     if (arr[j] > arr[i]) {
+  //       let temp = arr[i];
+  //       let tj = arr[j];
+  //       arr[i] = arr[j];
+  //       arr[j] = temp;
+  //       animations.push([i, j, temp, tj, "swap"]);
+  //     }
+  //   }
+  // }
 };
